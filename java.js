e61b2fa0ac672e4c95fdbeded12fb028c51598fe
pranav gosai <115298514+pranavgosai@users.noreply.github.com>
2022-12-03 18:15:46 +0530

    todo-lits

---
+++ java.js
@@ -0,0 +1,75 @@
+// // console.log("hello world" ,4+3);
+        
+// // console.warn("this is warning");
+// // console.error("thi is errr");
+// var number = 34;
+// var number2 = 56;
+// console.log(number+number2);
+// // 4. data types
+// // number 
+// var num1 = 455;
+// var num2 = 56.76;
+// // String 
+// var str1 = "this is a string";
+// var str2 = "this is a string";
+// // Object
+// var marks = {
+//     ravi:34,
+//     shubham: 78,
+//     harry:99.97,
+// }
+// console.log(marks);
+
+// var a = true;
+// var b = false;
+// console.log(a , b);
+// var und;
+// console.log(und);
+// var n= null;
+// console.log(n
+//     );
+    
+// var arr = [1,2,3,4,5]
+// console.log(arr);
+
+// var a = 5;
+// var b = 4;
+//  console.log("the value of a + b", a+b);
+//  console.log("the value of a - b", a-b);
+//  console.log("the value of a * b", a*b);
+//  console.log("the value of a / b", a/b);
+
+//  var c = b;
+//  c -= 1;
+//  console.log(c);
+// // //  comparison oprators
+//  var y = 34;
+//  var x = 56;
+
+//  console.log(x==y);
+//  console.log(x>=y);
+//  console.log(x<=y);
+ 
+
+// foncs
+
+// function avg(a,b){
+// return(a+b)/2;
+// }
+// c1 = avg(4,6);
+// c2 = avg(14,16);
+// c3 = avg(45,50);
+// console.log(c1,c2,c3);
+
+// var age = 4;
+// if(age>2){
+//     console.log('you are not kid');
+// }
+// else{
+//     console.log('you are a kid');
+// }
+// var arr = [1,2,3,4,5,6,7];
+// console.log(arr);
+// for(var i = 0; i<arr.length
+//     ; i++);
+// console.log(arr[i]);
